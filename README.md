683c2773c957ae5887f9b041546295bcb255eff6
NazarMural <nazarmural777@gmail.com>
2023-08-14 10:51:04 +0300

    finish

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # goit-node-hw-01-cli-app
+
+https://ibb.co/xY6fXpX
+https://ibb.co/hfmTzJ4
+https://ibb.co/bWbxWBr
+https://ibb.co/Gpw1wyV
---
+++ contacts.js
@@ -0,0 +1,60 @@
+const fs = require("fs/promises");
+const path = require("path");
+const nanoid = require("nanoid");
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+// TODO: задокументувати кожну функцію
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+    return contacts;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const contacts = await listContacts();
+    const contact = contacts.find((item) => item.id === contactId);
+    return contact;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const contacts = await listContacts();
+    const updatedContacts = contacts.filter((item) => item.id !== contactId);
+    await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 2));
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function addContact({ name, email, phone }) {
+  try {
+    const contacts = await listContacts();
+    const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+};
